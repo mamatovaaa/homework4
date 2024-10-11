c516a18062de237bf26c01769693387f87977fa4
mamatovaaa <mamatalievaajdana23@gmail.com>
2024-10-11 11:45:38 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+// console.log('hello world');
+// console.error('error')
+// console.warn('warn')
+// console.info('info')
+
+
+// alert( 'hello world' )
+
+// prompt('сенин атын ким?')
+
+// переменные ES var, ES6 let , const
+// ES - EcmaScript
+
+// var name = 'Сыргак'
+
+
+// var name = 'Айбарчын'
+
+// var name = 'Айдана'
+
+// console.log(name);
+
+
+// let name = 'сыргак'
+
+// name = 'Nurs'
+
+
+// console.log(name);
+
+
+// const age = 100
+
+
+// const age = 11
+
+
+// console.log(age);
+
+// const name = 'aidana'
+// const age = 18
+// const hobby = 'song'
+
+// // console.log(name, age, hobby); 1-жолу
+// console.log(`менин атым ${name}, жашым ${age}, хоббим ${hobby}`);
+// alert(`менин атым ${name}, жашым ${age}, хоббим ${hobby}`)
+
+
+
+// prompt("сенин атын ким")
+// prompt("канча жаштасын")
+
+
+
+const name = prompt('сенин атын ким')
+const age = prompt('канча жаштасын')
+const hobby = prompt('хоббин эмне')
+
+console.log(`менин атым ${name},жашым ${age}, хоббим ${hobby}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
